f2e3c2c66925d851c514e6cf293c739d385db5c1
Arthur Hippolyte <arthurhippolyte9000@gmail.com>
2017-03-10 14:41:30 +0000

    tiny turtle

---
+++ script.js
@@ -0,0 +1,15 @@
+TinyTurtle.apply(window);
+forward(40);
+right(90);
+forward(20);
+left(120);
+forward(20);
+left(120);
+forward(20);
+left(120);
+forward(20);
+right(90);
+forward(40);
+right(90);
+forward(20);
+stamp(1);
